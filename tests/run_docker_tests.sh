4ff3ba9fb3ecd25378c8f7db2a87d4377db0e453
Martin Clauß <mc@cs.uni-bonn.de>
2020-06-06 20:57:14 +0200

    add podman support; adjust usage of sudo

--- tests/run_docker_tests.sh
+++ tests/run_docker_tests.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
+container_cmd=""
+sudo=""
+
+if which docker &> /dev/null
+then
+	container_cmd=docker
+
+	if ! groups | grep docker
+	then
+		sudo="sudo"
+	fi
+elif which podman &> /dev/null
+then
+	container_cmd=podman
+else
+	echo "this script need either docker or podman to be installed... aborting"
+	exit 1
+fi
+
 for d in docker_fedora docker_ubuntu
 do
-    sudo docker build -v$(pwd)/../:/test -t $d - < $d
-    sudo docker run --rm $d
+    $sudo $container_cmd build -v$(pwd)/../:/test:Z -t $d - < $d
+    $sudo $container_cmd run --rm $d
 done
